00b5d4d2cc2d2c636f1596c7ed55374844ac5e0f
ExbomuUltra <exbomuultra@gmail.com>
2021-07-07 16:02:18 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -13,5 +13,10 @@ public class DiceGame {
         System.out.println("Die 2: " + dice2);
         System.out.println("Total value: " + (dice1 + dice2));
 
+        if (dice1 + dice2 > 7) {
+            System.out.println("You won!");
+        } else
+            System.out.println("You lost!");
+
     }
 }
