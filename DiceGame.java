4d3d5798615955ca297f032fa1fb14d43b4a263a
ExbomuUltra <exbomuultra@gmail.com>
2021-07-07 15:55:57 +0900

    Initial revision

---
+++ DiceGame.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DiceGame {
+
+    public static void main(String[] args) {
+        System.out.println("Rolling the dice...");
+        Random rand = new Random();
+        int dice1 = rand.nextInt(5) + 1;
+        int dice2 = rand.nextInt(5) + 1;
+
+        System.out.println("Die 1: " + dice1);
+        System.out.println("Die 2: " + dice2);
+        System.out.println("Total value: " + (dice1 + dice2));
+
+    }
+}
